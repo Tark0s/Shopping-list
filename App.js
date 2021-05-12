ce05dcc9cca96b059f1d90d47002b12c44313bcb
Lukas Tarkowski <tarkowski14@gmail.com>
2021-05-12 12:09:10 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,73 @@
+const Person = (props) => {
+  return (
+    <li>
+      <button onClick={props.delete}>-</button>
+      <span>{props.name}</span>
+    </li>
+  );
+};
+
+class List extends React.Component {
+  state = {
+    products: [
+      {
+        id: 1,
+        name: "Bread",
+      },
+      {
+        id: 2,
+        name: "Milk",
+      },
+      {
+        id: 3,
+        name: "Pasta",
+      },
+      {
+        id: 4,
+        name: "Spinach",
+      },
+      {
+        id: 5,
+        name: "Butter",
+      },
+      {
+        id: 6,
+        name: "Watermelon",
+      },
+      {
+        id: 7,
+        name: "Coffee",
+      },
+    ],
+  };
+
+  handleDelete(id) {
+    const products = [...this.state.products];
+    const index = products.findIndex((person) => person.id === id);
+    products.splice(index, 1);
+
+    this.setState({
+      products,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <ul>
+          {this.state.products.length > 0
+            ? this.state.products.map((product) => (
+                <Person
+                  name={product.name}
+                  key={product.id}
+                  delete={this.handleDelete.bind(this, product.id)}
+                />
+              ))
+            : "Wszystko masz już w koszyku!"}
+        </ul>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<List />, document.getElementById("root"));
